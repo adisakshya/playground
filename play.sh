297727f1a556541a59435c669d8780ab9514300d
adisakshya <adisakshya@triveous.com>
2020-07-26 12:33:59 +0530

    updated play-script

--- play.sh
+++ play.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
+# Start playground
 docker run \
     -p 8080:8080 \
     -v $(which docker):/usr/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$(pwd):/home/coder/project" \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    -v "$(pwd):/home/player/project" \
     -u $(id -u):$(id -g) \
     adisakshya/playground \
     --auth none
+
+# docker run \
+#     -p 8080:8080 \
+#     -v $(which docker):/usr/bin/docker \
+#     -v "//./pipe/docker_engine://./pipe/docker_engine" \
+#     -v "$(pwd):/home/player/project" \
+#     -u $(id -u):$(id -g) \
+#     adisakshya/playground \
+#     --auth none
