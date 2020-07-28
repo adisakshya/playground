c3ea1356d93d52c9fffb2592c79c1ef21aed5399
adisakshya <adisakshya@triveous.com>
2020-07-28 15:04:19 +0530

    updated play-script

--- play.sh
+++ play.sh
@@ -2,19 +2,12 @@
 
 # Start playground
 docker run \
+    -d \
+    --hostname playground \
     -p 8080:8080 \
-    -v $(which docker):/usr/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    -v "$(pwd):/home/player/project" \
+    -v "$(which docker)://usr/bin/docker" \
+    -v "$(pwd)://home/player/project" \
+    -v "//var/run/docker.sock://var/run/docker.sock:ro" \
     -u $(id -u):$(id -g) \
     adisakshya/playground \
     --auth none
-
-# docker run \
-#     -p 8080:8080 \
-#     -v $(which docker):/usr/bin/docker \
-#     -v "//./pipe/docker_engine://./pipe/docker_engine" \
-#     -v "$(pwd):/home/player/project" \
-#     -u $(id -u):$(id -g) \
-#     adisakshya/playground \
-#     --auth none
