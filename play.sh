5ddb6e91b3e32c35cf9712b9ebab0adfbfc1a057
adisakshya <adisakshya@triveous.com>
2020-07-30 00:38:14 +0530

    fix: play-script

--- play.sh
+++ play.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Start playground
-# docker run \
-#     -d \
-#     --hostname playground \
-#     -p 8080:8080 \
-#     -v "$(which docker)://usr/bin/docker" \
-#     -v "$(pwd)://home/player/project" \
-#     -v "//var/run/docker.sock://var/run/docker.sock:ro" \
-#     -u $(id -u):$(id -g) \
-#     adisakshya/playground \
-#     --auth none
+docker run \
+    -d \
+    --hostname playground \
+    -p 8080:8080 \
+    -v "$(which docker)://usr/bin/docker" \
+    -v "$(pwd)://home/player/project" \
+    -v "//var/run/docker.sock://var/run/docker.sock:ro" \
+    -u $(id -u):$(id -g) \
+    adisakshya/playground \
+    --auth none
 
 docker-compose -f reverse-proxy/docker-compose.yml up --build -d
-# cd ~/.local/share/code-server && sudo chown player:player .
